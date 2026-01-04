cd201980819b548e862e7f6bbe482c1133c562ee
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-01-04 13:19:16 +0530

    Time: 30 ms (38.13%), Space: 46.5 MB (33.08%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int maxSum = 0; 
+        for(int i = 0; i < nums.length; i++){
+            int cur = countDivisors(nums[i]);
+            maxSum += cur; 
+        }
+        return maxSum; 
+    }
+    public int countDivisors(int num){
+        int count = 0, sum = 0; 
+        for(int i = 1; i*i <= num; i++){
+            if(num%i==0){
+                count++; 
+                sum+= i; 
+                if((num/i)!=i){
+                    count++; 
+                    sum += (num/i);
+                }
+            }
+        }
+        if(count!=4) return 0; 
+        return sum; 
+    }
+}
