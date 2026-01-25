a64bc380a735d45fbb264636371b6ed7cd122a6f
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-01-25 10:20:08 +0530

    Time: 6 ms (7.43%), Space: 46.6 MB (5.47%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,40 +1,39 @@
 class Solution {
     public int compress(char[] chars) {
-        int ind = 0;  
+        int ind = 0; 
 
-        if (chars.length == 1) {
-            return 1;
+
+        if(chars.length==1){
+            return 1; 
         }
 
         String s = Character.toString(chars[0]);
 
-        for (int i = 1; i < chars.length; i++) {
-            if (chars[i] != chars[i - 1]) {
+        for(int i = 1; i < chars.length; i++){
 
+            if(chars[i]!=chars[i-1]){
                 chars[ind++] = s.charAt(0);
 
-
-                if (s.length() > 1) {
+                if(s.length() > 1){
                     String num = Integer.toString(s.length());
-                    for (int l = 0; l < num.length(); l++) {
+                    for(int l = 0; l < num.length();l++){
                         chars[ind++] = num.charAt(l);
                     }
-                }
 
+                }
                 s = "";
             }
-            s += chars[i];
+            s += chars[i];    
         }
-
-        // handle last group (SAME logic as above)
         chars[ind++] = s.charAt(0);
-        if (s.length() > 1) {
+        if(s.length() > 1){
             String num = Integer.toString(s.length());
-            for (int l = 0; l < num.length(); l++) {
+            for(int l = 0; l < num.length();l++){
                 chars[ind++] = num.charAt(l);
             }
         }
+        return ind; 
 
-        return ind;
+
     }
 }
