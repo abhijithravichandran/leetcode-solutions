d9de86b51df82e575593f8c1652b7442ccd3badc
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-01-25 14:18:29 +0530

    Time: 93 ms (86.83%), Space: 51.3 MB (45.81%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,20 +1,24 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n==1 || n==0) return 0; 
+        // Sieve of erathenes 
+        if(n==0 || n==1) return 0; 
         boolean[] primes = new boolean[n+1];
         Arrays.fill(primes,true);
         primes[0] = false; 
         primes[1] = false; 
-        // if(n <= 2) return 0; 
-        for(int i=2; i*i <= n; i++){
+
+        for(int i = 2; i*i <= n; i++){
+
             if(primes[i]){
-                for(int j = i*i; j <= n; j+=i){
-                    primes[j] = false; 
+
+                for(int p = i*i; p <= n;p+=i){
+                    primes[p] = false; 
                 }
             }
+
         }
         int count = 0; 
-        for(int i=0; i < n; i++){
+        for(int i = 2; i < n; i++){
             if(primes[i]) count++; 
         }
         return count; 
