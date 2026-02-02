4dd73b727db9cb6d472826532bbb53f62747f460
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-02-02 20:01:35 +0530

    Time: 1 ms (99.21%), Space: 45.7 MB (31.75%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -1,9 +1,24 @@
 class Solution {
     public int minimumCost(int[] nums) {
         int sum = nums[0];
-        nums[0] = Integer.MAX_VALUE; 
-        Arrays.sort(nums);
-        sum += nums[0] + nums[1];
+        int min1 = Integer.MAX_VALUE; 
+        int min2 = Integer.MAX_VALUE; 
+        for(int i = 1; i < nums.length; i++){
+            
+            if(nums[i] < min1){
+                min2 = min1; 
+                min1 = nums[i];
+            }
+            else if(nums[i] < min2){
+                min2 = nums[i];
+            }
+        }
+
+        sum += min1 + min2; 
+
         return sum; 
     }
 }
+
+// Why that if-else condition, because at the start we get the lowest element, then the first condition 
+//  won't get executed, so we may miss the second-lowest element. So, to handle that, the second if-else condition. 
