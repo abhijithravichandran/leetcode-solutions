a4640502c02c12c6ca59ea4d522a8a6816c2e18a
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-02-06 14:08:48 +0530

    Time: 2 ms (9.34%), Space: 44.9 MB (27.67%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length, ind = -1;;
+        int min = Integer.MAX_VALUE, minInd = -1; 
+         for(int i = n-2; i >= 0; i--){
+
+            if(nums[i] < nums[i+1]){
+                ind = i; 
+                break; 
+            }
+         }
+
+        if(ind!=-1){ 
+        for(int i = n-1; i > ind; i--){
+            if(nums[i] > nums[ind]){
+                swap(nums,ind,i);
+                break; 
+            }
+        }
+    }
+
+        Arrays.sort(nums,ind+1,n);
+
+    }
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp; 
+    }
+}
