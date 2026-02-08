053e3312984a7ef11217144569424e53c0e07bf1
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-02-08 11:31:36 +0530

    Time: 111 ms (31.44%), Space: 140.5 MB (93.13%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -11,7 +11,7 @@ class NumMatrix {
         for(int i = 0; i < n;i++){
             for(int j = 0; j < m; j++){
                 
-                int above = (i > 0) ? matrix[i-1][j] : 0; 
+                int above = (i > 0) ? matrix[i-1][j]: 0;
                 int left = (j > 0) ? matrix[i][j-1] : 0; 
                 int common = (i > 0 && j > 0) ? matrix[i-1][j-1] : 0; 
 
@@ -23,9 +23,9 @@ class NumMatrix {
     public int sumRegion(int row1, int col1, int row2, int col2) {
         if(row1==0 && col1==0) return matrix[row2][col2];
 
-        int above = (row1 > 0) ? matrix[row1-1][col2] : 0;
+        int above = (row1 > 0) ? matrix[row1-1][col2] : 0; 
         int left = (col1 > 0) ? matrix[row2][col1-1] : 0; 
-        int common = (col1 > 0 && row1 > 0) ? matrix[row1-1][col1-1] : 0; 
+        int common = (row1 > 0 && col1 > 0 ) ? matrix[row1-1][col1-1] : 0; 
 
         return matrix[row2][col2] - above - left + common; 
     }
