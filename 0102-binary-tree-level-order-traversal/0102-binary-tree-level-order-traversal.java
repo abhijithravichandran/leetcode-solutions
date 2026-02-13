29522ff15e79430160824cded4977cf6251e72dc
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-02-13 19:30:28 +0530

    Time: 1 ms (95.53%), Space: 47.2 MB (6.11%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,32 +15,28 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
         if(root==null) return res; 
         q.add(root);
 
-
         while(!q.isEmpty()){
             List<Integer> level = new ArrayList<>();
-
             int n = q.size();
 
-            for(int i = 0; i < n;i++){
+            for(int i = 0;i < n; i++){
                 TreeNode temp = q.poll();
-
-                level.add(temp.val);
 
                 if(temp.left!=null) q.add(temp.left);
                 if(temp.right!=null) q.add(temp.right);
 
+                level.add(temp.val);
             }
 
             res.add(level);
-            
         }
         return res; 
+
+
     }
 }
-
-// This code-thought process is soo good, and I just admire it, because the level order has been done at great level... 
