adf8c144adb27ff2d0ffc9b043d094382ca9d957
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-02-15 15:11:33 +0530

    Time: 1 ms (94.72%), Space: 43.4 MB (86.14%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,31 +1,26 @@
 class Solution {
-    public List<List<Integer>> generate(int rows) {
-        List<List<Integer>> res = new ArrayList<>(new ArrayList<>());
-        
-        if(rows==0) return res; 
-        else if(rows==1){
-            res.add(Arrays.asList(1));
-            return res; 
-        }
-        else if(rows==2){
-            res.add(Arrays.asList(1));
-            res.add(Arrays.asList(1,1));
-            return res; 
-        }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if(numRows==0) return res; 
         res.add(Arrays.asList(1));
+        if(numRows==1) return res; 
         res.add(Arrays.asList(1,1));
+        if(numRows==2) return res; 
 
-        for(int i = 2; i < rows; i++){
-
+        for(int i = 2; i < numRows; i++){
             List<Integer> row = new ArrayList<>();
             row.add(1);
-            for(int j = 1; j < res.get(i-1).size();j++){
-                int sum = res.get(i-1).get(j) + res.get(i-1).get(j-1);
+            int sum = 0; 
+            for(int j = 0; j < res.get(i-1).size()-1;j++){
+                sum = res.get(i-1).get(j) + res.get(i-1).get(j+1);
                 row.add(sum);
             }
+
             row.add(1);
             res.add(row);
         }
+
         return res; 
     }
 }
