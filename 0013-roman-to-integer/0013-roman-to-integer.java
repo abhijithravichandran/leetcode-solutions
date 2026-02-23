8a31e8442028bff6470eaac31e57f21b7d0283c4
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-02-23 14:20:44 +0530

    Time: 3 ms (76.86%), Space: 46.5 MB (76.26%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,30 +2,53 @@ class Solution {
     public int romanToInt(String s) {
         HashMap<Character,Integer> map = new HashMap<>();
         int n = s.length();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
+        // map.put('I', 1);
+        // map.put('V', 5);
+        // map.put('X', 10);
+        // map.put('L', 50);
+        // map.put('C',100);
+        // map.put('D',500);
+        // map.put('M',1000);
 
         int sum = 0; 
-        if(s.length() < 2) return map.get(s.charAt(0));
+        // if(s.length() < 2) return map.get(s.charAt(0));
 
-        for(int i = 0; i < s.length()-1;i++){
-            char ch1 = s.charAt(i);
-            char ch2 = s.charAt(i+1);
-            if(map.get(ch1) < map.get(ch2)){
-                sum -=  map.get(ch1);
+        // for(int i = 0; i < s.length()-1;i++){
+        //     char ch1 = s.charAt(i);
+        //     char ch2 = s.charAt(i+1);
+        //     if(map.get(ch1) < map.get(ch2)){
+        //         sum -=  map.get(ch1);
+        //     }
+        //     else{
+        //         sum += map.get(ch1);
+        //     }
+        // }
+        // sum += map.get(s.charAt(s.length()-1));
+
+
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            int cur = value(ch);
+            if(i < n-1 && cur < value(s.charAt(i+1))){
+                sum -= value(ch);
             }
             else{
-                sum += map.get(ch1);
+                sum += value(ch);
             }
         }
-        sum += map.get(s.charAt(s.length()-1));
         
         return sum; 
+
+    }
+    public int value(char ch){
+        if(ch=='I') return 1; 
+        else if(ch=='V') return 5; 
+        else if(ch=='X') return 10; 
+        else if(ch=='L') return 50;
+        else if(ch=='C') return 100; 
+        else if(ch=='D') return 500;
+        return 1000;
+
 
     }
 }
