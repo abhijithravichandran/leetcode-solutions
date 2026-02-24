48568414c6320dad08c5f817068346ead94f18b7
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-02-24 18:47:37 +0530

    Time: 32 ms (76.77%), Space: 59 MB (90.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,35 +2,42 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length; 
-        HashMap<List<Integer>,Integer> map = new HashMap<>();
 
         List<List<Integer>> res = new ArrayList<>();
-        for(int k = 0; k < n-2; k++){
-            int i = k+1, j = n-1;
-            while(i < j){
-                int num = nums[k]+nums[i]+nums[j];
-                List<Integer> row = new ArrayList<>();
-                row.add(nums[k]);
-                row.add(nums[i]);
-                row.add(nums[j]);
-                if(num==0 && !map.containsKey(row)){
-                    res.add(row);
-                    map.put(row,1);
-                    i++; 
-                    j--; 
-                }
-                else if(num > 0){
-                    j--; 
-                }
-                else if(num < 0){
-                    i++; 
-                }
-                else {
-                    i++; 
-                    j--; 
-                }
 
+        for(int k = 0; k < n-2; k++){
+            int i = k+1; 
+            int j = n-1; 
+
+            if(k > 0 && nums[k]==nums[k-1]) continue; // After seeing this line, and the complete logic, it is so amazing and magical 
+
+            // This code is so great, because they handled the duplicates in the logic whereas I was struggling with hashmap. This code is so great and next level-- It's beyond my thinking for now.. It is so good and great.... 
+
+            while(i < j){
+                int sum = nums[k]+nums[i]+nums[j];
+                int left = nums[i];
+                int right = nums[j];
+
+                if(sum==0){
+                    res.add(Arrays.asList(nums[k],nums[i],nums[j]));
+                    i++; 
+                    j--; 
+
+                    // Skip duplicates for left
+                    while(i < j &&  nums[i]==nums[i-1])i++; 
+
+                    // Skip duplicates for right
+                    while(j > i && nums[j]==nums[j+1]) j--; 
+
+                }
+                else if(sum < 0){
+                    i++; 
+                }
+                else{
+                    j--;
+                }
             }
+            
         }
         return res; 
     }
