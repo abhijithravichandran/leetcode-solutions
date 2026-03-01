75a0d05268d9a53641a4cd01aba1ac0f74e3c414
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-03-01 22:42:05 +0530

    Time: 16 ms (84.58%), Space: 47.5 MB (80.74%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -3,18 +3,29 @@ class Solution {
         HashMap<Integer,Integer> map= new HashMap<>();
 
         int n = nums.length; 
-
-        for(int num: nums){
-           if(num%2==0) map.put(num,map.getOrDefault(num,0)+1);
-        }
         int ele = -1; 
         int cnt= 0; 
-        for(int num: map.keySet()){
-            if(map.get(num) > cnt || (cnt==map.get(num) && num < ele)){
-                ele = num; 
-                cnt = map.get(num);
+
+        for(int num: nums){
+           if(num%2==0) {
+            map.put(num,map.getOrDefault(num,0)+1);
+            int curCnt = map.get(num);
+            int curEle = num; 
+            if(curCnt > cnt || (curCnt==cnt && curEle < ele)){
+                ele = curEle; 
+                cnt = curCnt; 
             }
+           }
+
         }
+        // int ele = -1; 
+        // int cnt= 0; 
+        // for(int num: map.keySet()){
+        //     if(map.get(num) > cnt || (cnt==map.get(num) && num < ele)){
+        //         ele = num; 
+        //         cnt = map.get(num);
+        //     }
+        // }
         return ele; 
 
     }
