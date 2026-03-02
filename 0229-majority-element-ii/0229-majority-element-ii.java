db54c8b27aeaff5f4c2ccf9b6aa871c05b35c742
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-03-02 08:50:45 +0530

    Time: 2 ms (99.71%), Space: 52.7 MB (51.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,34 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length; 
         int ele1 = 0, ele2 = 0; 
         int cnt1 = 0, cnt2 = 0; 
 
         for(int num: nums){
-            if(cnt1==0 && num!=ele2){
-                cnt1 = 1; 
+            if(cnt1 == 0 && num!=ele2){
                 ele1 = num; 
+                cnt1 = 1; 
             }
-            else if(cnt2==0 && num!=ele1){
-                cnt2 = 1; 
+            else if(cnt2 == 0 && num!=ele1){
                 ele2 = num; 
+                cnt2 = 1; 
             }
-            else if(ele1==num) cnt1++; 
-            else if(ele2==num) cnt2++; 
+            else if(num==ele1){
+                cnt1++; 
+            }
+            else if(num==ele2){
+                cnt2++; 
+            }
             else{
                 cnt1--; 
                 cnt2--; 
             }
         }
+        List<Integer> list = new ArrayList<>();
+        int count1 =0, count2 = 0; 
 
-        int count1 = 0, count2 = 0; 
         for(int num: nums){
             if(num==ele1) count1++; 
             if(num==ele2) count2++; 
-    }
-        List<Integer> list = new ArrayList<>();
-        int n = nums.length; 
-        if(count1 > n/3 ) list.add(ele1);
+        }
+
+        if(count1 > n/3) list.add(ele1);
         if(count2 > n/3 && ele1!=ele2) list.add(ele2);
+
         return list; 
+        
     }
 }
