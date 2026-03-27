0ab4ced1fee683618d772f770ee41493613f56b6
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-03-27 10:19:08 +0530

    Time: 2 ms (70.22%), Space: 47.3 MB (81.46%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        
+        boolean isDigit = s.matches("\\d+");
+
+        List<String> list = new ArrayList<>();
+        if(isDigit) {
+            list.add(s);
+            return list; 
+        }
+        char[] arr = s.toCharArray();
+        casePermutation(arr,list,0);
+        return list; 
+
+    }
+
+    public void casePermutation(char[] arr, List<String> list, int ind){
+        
+        if(ind < arr.length && Character.isDigit(arr[ind])){
+            while(ind < arr.length && Character.isDigit(arr[ind])) ind++; 
+        }
+
+        if(ind >= arr.length){
+            list.add(String.valueOf(arr));
+            return; 
+        }
+
+        char ch = arr[ind];
+
+        if(Character.isUpperCase(ch)){
+            arr[ind] = Character.toLowerCase(ch);
+        }
+        else arr[ind] = Character.toUpperCase(ch);
+        casePermutation(arr,list,ind+1);
+
+    
+        arr[ind] = ch; 
+        casePermutation(arr,list,ind+1);
+    }
+}
