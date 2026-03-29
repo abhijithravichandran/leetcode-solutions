61a9424629b93407a357c00e795c574ac3292149
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-03-29 18:26:32 +0530

    Time: 4 ms (50.84%), Space: 44.5 MB (80%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -4,31 +4,37 @@ class Solution {
         int[] global = new int[26];
 
         for(int i = 0; i < words[0].length();i++){
-            char ch = words[0].charAt(i);
-            global[ch-'a']++; 
+            char ch  = words[0].charAt(i);
+            global[ch-'a']++;
         }
 
         for(int i = 1; i < n; i++){
-            int[] current = new int[26];
-            for(char c: words[i].toCharArray()){
-                current[c-'a']++; 
+            
+            int[] temp = new int[26];
+            for(int j = 0; j < words[i].length();j++){
+                char ch = words[i].charAt(j);
+                temp[ch-'a']++;
             }
 
             for(int j = 0; j < 26; j++){
-                global[j] = Math.min(global[j],current[j]);
+                global[j] = Math.min(global[j],temp[j]);  //This is the amazing line ever. 
             }
         }
 
-        List<String> res =new ArrayList<>();
+        List<String> res = new ArrayList<>();
+
         for(int i = 0; i < 26; i++){
-            if(global[i]==1) res.add(""+(char)(i+'a'));
-            else if(global[i] > 1){
-                for(int j = 0; j < global[i];j++){
-                    res.add(""+(char)(i+'a'));
+            if(global[i] ==1) res.add(String.valueOf((char)('a'+i)));
+            else{
+                while(global[i] > 0){
+                    global[i]--;
+                    res.add(String.valueOf((char)('a'+i)));
                 }
             }
         }
 
         return res; 
+
+
     }
 }
