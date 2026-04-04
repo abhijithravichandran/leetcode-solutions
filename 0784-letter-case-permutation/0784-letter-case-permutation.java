be6f1aa6fe4f507aab6d0a5091e12854e3153560
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-04-04 15:55:07 +0530

    Time: 1 ms (99.89%), Space: 47.4 MB (53.15%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -1,32 +1,30 @@
 class Solution {
     public List<String> letterCasePermutation(String s) {
-        char[] arr = s.toCharArray();
+        char[] str = s.toCharArray();
         List<String> res = new ArrayList<>();
-        permutations(arr,res,0);
+        permutations(str,res,0);
         return res; 
+
     }
 
     public void permutations(char[] arr, List<String> res, int ind){
 
-        while(ind < arr.length && !Character.isLetter(arr[ind])){
+        while(ind < arr.length && Character.isDigit(arr[ind])){
             ind++; 
         }
-        
+
         if(ind==arr.length){
             res.add(new String(arr));
             return; 
         }
 
-        
-        
-
         char ch = arr[ind];
 
-        if(Character.isUpperCase(ch)) arr[ind] = Character.toLowerCase(ch);
-        else if(Character.isLowerCase(ch)) arr[ind] = Character.toUpperCase(ch);
+        if(Character.isLowerCase(ch)) arr[ind] = Character.toUpperCase(ch);
+        else arr[ind] = Character.toLowerCase(ch);
         permutations(arr,res,ind+1);
 
         arr[ind] = ch; 
         permutations(arr,res,ind+1);
-    }
+    }   
 }
