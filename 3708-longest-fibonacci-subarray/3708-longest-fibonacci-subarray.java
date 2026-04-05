558c6784ef1aef8c5fde73187ac7c8d4c240afd2
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-04-05 07:23:47 +0530

    Time: 1 ms (100%), Space: 98.3 MB (38.96%) - LeetHub

--- 3708-longest-fibonacci-subarray/3708-longest-fibonacci-subarray.java
+++ 3708-longest-fibonacci-subarray/3708-longest-fibonacci-subarray.java
@@ -2,29 +2,17 @@ class Solution {
     public int longestSubarray(int[] nums) {
         int n = nums.length; 
 
-        int len = 0, cnt = 0; 
-
-
+        int cnt = 2, maxLen = 2; 
         for(int i = 2; i < n; i++){
-            int third = nums[i-1] + nums[i-2]; 
-            if(nums[i]==third){
-                if(cnt==0){
-                    cnt = 2; 
-                }
-                else{
-                    cnt++; 
-                }
+
+            if(nums[i]==nums[i-1]+nums[i-2]){
+                cnt++; 
+                maxLen = Math.max(maxLen,cnt);
             }
             else{
-                cnt = 0; 
+                cnt = 2; 
             }
-  
-            len = Math.max(len,cnt+1);
         }
-
-        if(len==1) return 2; 
-
-        return len; 
-
+        return maxLen; 
     }
 }
