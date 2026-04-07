97f39f16547b8b44eaf3f546e80668782d592421
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-04-07 14:47:18 +0530

    Time: 0 ms (100%), Space: 42.8 MB (84.41%) - LeetHub

---
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int maxSum = nums[0], curSum = nums[0], n = nums.length; 
+
+        for(int i = 1; i < n; i++){
+            
+            if(nums[i] <= nums[i-1]){
+                curSum = nums[i];
+            }
+            else{
+                curSum += nums[i];
+            }
+
+            maxSum = Math.max(maxSum,curSum);
+            
+        }
+
+        return maxSum; 
+    }
+}
