9cdbccfbf7a09b89f3cffca7209c52b8c1bd78d3
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-04-08 21:14:15 +0530

    Time: 0 ms (100%), Space: 43.9 MB (26.55%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -4,10 +4,16 @@ class Solution {
 
         // Simply-I'm taking the possible minimum and reducing the search-space. 
         // Just reducing the search space - because in rotated array, either half will be sorted 
+        // I easliy sovled it - because I know the data that - either half is guaranteed to be sorted
 
         while(low <= high){
 
             int mid = low + (high-low)/2;
+
+            if(arr[low] <= arr[high]){
+                min = Math.min(arr[low],min);
+                break; 
+            }
 
             if(arr[low] <= arr[mid]){
                 min = Math.min(arr[low], min);
