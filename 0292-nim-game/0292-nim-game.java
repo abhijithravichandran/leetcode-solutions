d615d1e7f0c6e2074525d933c1c3895dd41c0503
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-04-18 16:00:49 +0530

    Time: 0 ms (100%), Space: 42 MB (76.16%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if(n <= 3) return true; 
-        // Case 1: Taking 3 and winning 
-        // if(n%3==0) return true; 
-        
-        // // Case 2: Taking 2 and winning 
-        // int turn = n/2, rem = n%3;
-        // if(turn%2==0 && (rem <= 3)) return true; 
-
-        // // Case 3: Taking 1 and winning
-        
-
-        // return false; 
-
         if(n%4==0) return false; 
 
         return true; 
