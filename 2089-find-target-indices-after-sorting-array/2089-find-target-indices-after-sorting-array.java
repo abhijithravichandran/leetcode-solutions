a983d10578f68835e7f2d584472ab9107386b1ac
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-05-03 11:36:15 +0530

    Time: 0 ms (100%), Space: 44.4 MB (98.37%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,29 +1,42 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        int low = 0, high = nums.length-1, startInd = -1; 
-        ArrayList<Integer> indices = new ArrayList<>();
+        // int low = 0, high = nums.length-1, startInd = -1; 
+        // ArrayList<Integer> indices = new ArrayList<>();
 
-        while(low <= high){
+        // while(low <= high){
 
-            int mid = low + (high-low)/2;
+        //     int mid = low + (high-low)/2;
 
-            if(nums[mid] >= target){
-                if(nums[mid]==target){
-                    startInd = mid; 
-                }
-                high = mid - 1; 
-            }
-            else{
-                low = mid + 1; 
-            }
+        //     if(nums[mid] >= target){
+        //         if(nums[mid]==target){
+        //             startInd = mid; 
+        //         }
+        //         high = mid - 1; 
+        //     }
+        //     else{
+        //         low = mid + 1; 
+        //     }
+        // }
+        // if(startInd == -1) return indices; 
+        // for(int i = startInd; i < nums.length; i++){
+        //     if(nums[i]==target) indices.add(i);
+        // }
+
+        // return indices; 
+
+        int less = 0, equal = 0; 
+
+        for(int i: nums){
+            if(i==target) equal++; 
+            else if(i < target) less++; 
         }
-        if(startInd == -1) return indices; 
-        for(int i = startInd; i < nums.length; i++){
-            if(nums[i]==target) indices.add(i);
+        List<Integer> res = new ArrayList<>();
+        for(int i = less; i < less+equal; i++){
+            res.add(i);
         }
 
-        return indices; 
+        return res; 
     }
 }
