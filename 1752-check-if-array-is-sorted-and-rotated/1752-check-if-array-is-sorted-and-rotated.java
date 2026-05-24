dccb70f06abc75d0fcd2be9ca481cdbf211f07c8
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-05-24 08:14:14 +0530

    Time: 0 ms (100%), Space: 43 MB (44.91%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -10,3 +10,23 @@ class Solution {
         return count <= 1;
     }
 }
+
+/*
+A sorted rotated array can have only one place where order breaks.
+
+Example:
+
+[3,4,5,1,2]
+
+Look carefully:
+
+3 < 4
+4 < 5
+5 > 1   ← break
+1 < 2
+2 < 3   ← circular comparison
+
+There is ONLY ONE descending point.
+
+That’s the entire problem.
+*/
