981a39ce944d4bdcb89b77fe03f0211c6bc21155
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-05-26 14:32:30 +0530

    Time: 2 ms (91.71%), Space: 47.9 MB (36.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j =0;
+        int j = 0; 
         for(int i = 0; i < nums.length; i++){
             if(nums[i]!=0){
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                j++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp; 
+                j++; 
             }
         }
+        
     }
 }
+
+// This code is amazing, and I still kind of have hard time truly get it. I still feel like I don't fully get it.. And this swapping, and mainly the checking is so great and so amazing.... 
