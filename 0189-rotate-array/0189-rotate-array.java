0de1eec1b2ab1fb049c99037a9d1f4bf6df59b4b
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-08-03 11:23:43 +0530

    Time: 2 ms (18.95%), Space: 61.4 MB (41.42%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,28 +2,26 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length; 
 
+        if(n==0) return; 
+
         k = k%n; 
 
-        // if(n==0) return;
+        if(k==0) return; 
 
         rev(nums,n-k,n-1);
         rev(nums,0,n-k-1);
         rev(nums,0,n-1);
-       
+
     }
 
     public void rev(int[] arr, int i, int j){
-        
+
         while(i < j){
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp; 
             i++; 
-            j--;
+            j--; 
         }
     }
 }
-
-// Note: 
-    // Every element moves
-    // newIndex = (oldIndex + k) % n
