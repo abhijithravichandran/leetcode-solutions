22a6e5356b326cee87c3b676f9c23363368dcc32
Abhijith <149925874+abhijithravichandran@users.noreply.github.com>
2026-09-18 13:49:57 +0530

    Time: 0 ms (100%), Space: 43 MB (36.65%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int n = palindrome.length();
+        if(n==1) return "";
+        char c = 'a';
+        StringBuilder sb = new StringBuilder(palindrome);
+
+
+        for(int i = 0; i < (n/2); i++){
+            char ch = palindrome.charAt(i);
+            if(ch!=c){
+                sb.setCharAt(i,c);
+                c++; 
+                break; 
+            }
+        }
+
+        if(c=='a'){
+            if(sb.charAt(n-1)=='a') c++; 
+            sb.setCharAt(n-1,c);
+        }
+
+        return sb.toString();
+    }
+}
